7f35443356ffe7089660291b0a21ea391388300f
Lauterjung <miguelbusarello@hotmail.com>
2022-02-23 16:11:53 -0300

    Added sideways_shooter folder (DIY book session)

---
+++ old_tests/sideways_shooter/classes/settings.py
@@ -0,0 +1,18 @@
+class Settings:
+    def __init__(self):
+        
+        # screen
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.full_screen = False    
+        
+        # ship   
+        self.ship_speed = 1.5
+
+        # bullet
+        self.bullet_speed = 1.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
